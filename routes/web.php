634e14ae6af5e238502e136c46ef7898a23d0cb6
Lexuil <ojumanab@unal.edu.co>
2022-06-21 19:45:18 -0500

    Adding a dropdown menu with javascript

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ Route::get('/', function () {
 
     return view('posts', [
         'posts' => Post::latest()->get(),
+        'categories' => Category::all()
     ]);
 });
 
@@ -33,12 +34,15 @@ Route::get('post/{post:slug}', function (Post $post) {
 
 Route::get('categories/{category:slug}', function (Category $category) {
     return view('posts', [
-        'posts' => $category->posts
+        'posts' => $category->posts,
+        'currentCategory' => $category,
+        'categories' => Category::all()
     ]);
 });
 
 Route::get('authors/{author:username}', function (User $author) {
     return view('posts', [
-        'posts' => $author->posts
+        'posts' => $author->posts,
+        'categories' => Category::all()
     ]);
 });
