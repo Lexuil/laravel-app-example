a7441a94cc31937a3933d0ccd824b4b59a889757
Lexuil <ojumanab@unal.edu.co>
2022-06-29 15:05:35 -0500

    Create newsletter system

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
 use App\Http\Controllers\CommentController;
+use App\Http\Controllers\NewsletterController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionController;
+use App\Services\Newsletter;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 
@@ -18,3 +21,5 @@ Route::get('login', [SessionController::class, 'create'])->middleware('guest');
 Route::post('login', [SessionController::class, 'store'])->middleware('guest');
 
 Route::post('logout', [SessionController::class, 'destroy'])->middleware('auth');
+
+Route::post('/newsletter', [NewsletterController::class, 'subscribe']);
